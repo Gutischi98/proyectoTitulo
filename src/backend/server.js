423795b692a78a3ea4fd595a951934ffd76bdbef
guti <gustavo.devti98@gmail.com>
2026-01-18 00:39:05 -0300

    Fix: Configuración de server.js para servir el front

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const db = require('./config/db'); 
+
 const inventoryRoutes = require('./routes/inventoryRoutes');
 const authRoutes = require('./routes/authRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
@@ -12,6 +14,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.use('/api/equipos', inventoryRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
@@ -19,7 +24,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Sistema de Inventario Local Funcionando');
+    res.sendFile(path.join(__dirname, '../frontend/login.html'));
 });
 
 app.get('/api/test', (req, res) => {
@@ -30,5 +35,5 @@ app.get('/api/test', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log(`Servidor corriendo en puerto ${port}`);
 });
